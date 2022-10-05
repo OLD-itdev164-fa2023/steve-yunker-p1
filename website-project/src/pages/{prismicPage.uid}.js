65fdb2d40bfbeee6017100df3a870e4b9eff496b
Steve Yunker <syunker@weimerbearing.com>
2022-10-05 15:20:12 -0500

    set up dynamic content on pages

---
+++ website-project/src/pages/{prismicPage.uid}.js
@@ -0,0 +1,42 @@
+﻿import * as React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import {PrismicRichText} from '@prismicio/react'
+
+
+const Page = (props) => {
+    console.log(props)
+    return (
+    <Layout>
+      <Seo title="Page two" />
+      <PrismicRichText 
+      field={props.data.prismicPage.data.page_title.richText} 
+      />
+      <PrismicRichText 
+      field={props.data.prismicPage.data.content.richText} 
+      />
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PageQuery($id: String) {
+    prismicPage(id: { eq: $id }) {
+      data {
+            content {
+                richText
+            }
+        page_title {
+          text
+          richText
+        }
+      }
+    }
+  }
+`
+
+export default Page
